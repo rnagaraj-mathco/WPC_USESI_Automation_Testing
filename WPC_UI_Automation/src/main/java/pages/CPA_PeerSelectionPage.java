81871ffb0b4eb5f407ea97cf93a1e5481cc571e7
NagarajR <NagarajR@LAPTOP-TBP3F2U4>
2025-06-23 10:11:20 +0530

    Completed Automation Testing for the Peer Selection - 01

--- WPC_UI_Automation/src/main/java/pages/CPA_PeerSelectionPage.java
+++ WPC_UI_Automation/src/main/java/pages/CPA_PeerSelectionPage.java
@@ -31,22 +31,43 @@ public class CPA_PeerSelectionPage {
     private WebElement waitForElement(By locator) {
         return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
-    
+    //Peer Selection - 01
     //Select Target Customer for Peer Matching table
     //Entire Table
-    By CPA_SelectTargetCustomerTable = By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[3]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/form[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[3]");
+    By CPA_PS1_SelectTargetCustomerTable = By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[3]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/form[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[3]");
     //First row record
-    By CPA_SelectTargetCustomerTableFirstRow = By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[3]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/form[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[3]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/span[1]/span[1]/input[1]");
+    By CPA_PS1_SelectTargetCustomerTableFirstRow = By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[3]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/form[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[3]/table[1]/tbody[1]/tr[1]/td[1]");
     //SearchField 
-    By CPA_SearchKeywordSelectTargetCustomer = By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[3]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/form[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/input[1]");
+    By CPA_PS1_PS1_SearchKeywordSelectTargetCustomer = By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[3]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/form[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/input[1]");
     //Select Customer Button
-    By CPA_SelectCustomerBtn = By.xpath("");
-   //Loading the Peer Selection Screen
+    By CPA_PS1_SelectCustomerBtn = By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[3]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/form[1]/div[1]/div[5]/div[1]/div[1]/button[1]");
+    //Back button
+    By CPA_PS1_BackBtn = By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[3]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/form[1]/div[1]/div[4]/div[1]/div[1]/button[1]");
+    //Geographical Map 
+    By CPA_GeographicalMap = By.xpath("/html/body/div[1]/div/div/div/div[2]/div[1]/div[2]/div[3]/div[3]/div/div/div/div/div/div/div[3]/div/div/div/div[2]/div/div/div/div");
+    //Download button
+    By CPA_PS1_DownloadBtn_SelectTargetCustomerTable = By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[3]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/form[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/span[1]/button[1]");
+    
+    //Peer Selection -02 
+    //Comparative Peer Group Selection table
+    By CPA_ComparativePeerGroupSelectionTable = By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[3]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/form[1]/div[1]/div[6]/div[1]");
+    //Back button
+    By CPA_PS2_BackBtn = By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[3]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/form[1]/div[1]/div[9]/div[1]/div[1]/button[1]");
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //Loading the Peer Selection Screen
     public void navigateTo() throws IOException, InterruptedException {
     	//This gets the URL of the Branch Customer Selection Screen
   		driver.get(ConfigReader.cpa_peerSelection());
   		//Function waits till the Entire Overview Screen is getting loaded
-  		waitForElement(CPA_SelectTargetCustomerTable);
+  		waitForElement(CPA_PS1_SelectTargetCustomerTable);
   	    System.out.println("--- Loaded the Peer Selection -01 Screen where Target customer will be selected for Peer Matching");
   		Thread.sleep(2000);
   		// Take screenshot after the page is fully ready
@@ -65,22 +86,22 @@ public class CPA_PeerSelectionPage {
     //SearchField of Select Target Customer for Peer Matching table
     
     public void searchForKeyword_STC() throws IOException, InterruptedException {
- 	   WebElement SearchKeywordSelectTargetCustomer = waitForElement(CPA_SearchKeywordSelectTargetCustomer);
- 	  SearchKeywordSelectTargetCustomer.click();
- 	  SearchKeywordSelectTargetCustomer.clear();
- 	  SearchKeywordSelectTargetCustomer.sendKeys("Nagaraj");
+ 	   WebElement PS1_SearchKeywordSelectTargetCustomer = waitForElement(CPA_PS1_PS1_SearchKeywordSelectTargetCustomer);
+ 	  PS1_SearchKeywordSelectTargetCustomer.click();
+ 	  PS1_SearchKeywordSelectTargetCustomer.clear();
+ 	  PS1_SearchKeywordSelectTargetCustomer.sendKeys("Nagaraj");
  	   System.out.println("--- Searched: Keyword search was done");
  	    Thread.sleep(5000); 
  	 // Take screenshot after the keyword is searched
          TakesScreenshot screenshot = (TakesScreenshot) driver;
          File sourcefile = screenshot.getScreenshotAs(OutputType.FILE);
-         File screenshotPath = new File("src/test/resources/screenshots/CustomerPeerAnalysis/Selections/PeerSelections/SearchKeywordSelectTargetCustomer.png");
+         File screenshotPath = new File("src/test/resources/screenshots/CustomerPeerAnalysis/Selections/PeerSelections/PS1_SearchKeywordSelectTargetCustomer.png");
          FileHandler.copy(sourcefile, screenshotPath);
          	
          //Clearing the Search Keyword
-         SearchKeywordSelectTargetCustomer.click();
-         SearchKeywordSelectTargetCustomer.sendKeys(Keys.CONTROL + "a");
-         SearchKeywordSelectTargetCustomer.sendKeys(Keys.BACK_SPACE);
+         PS1_SearchKeywordSelectTargetCustomer.click();
+         PS1_SearchKeywordSelectTargetCustomer.sendKeys(Keys.CONTROL + "a");
+         PS1_SearchKeywordSelectTargetCustomer.sendKeys(Keys.BACK_SPACE);
          Thread.sleep(2000);
        //This is used to come out of the dropdown overlay effect and click the next locators
 //   		Actions actions = new Actions(driver);
@@ -92,18 +113,33 @@ public class CPA_PeerSelectionPage {
     //Select Target Customer Button with selection
   	public void selectCustomerBtnWithSelection() throws IOException, InterruptedException{
   		//finds the first row of the table and clicks as the button gets activated
-  		WebElement SelectTargetCustomerTableFirstRow = waitForElement(CPA_SelectTargetCustomerTableFirstRow);
+  		WebElement PS1_SelectTargetCustomerTableFirstRow = waitForElement(CPA_PS1_SelectTargetCustomerTableFirstRow);
   		//waits for the Select Customer button to get load
-  		WebElement SelectCustomerBtn = waitForElement(CPA_SelectCustomerBtn);
+  		WebElement PS1_SelectCustomerBtn = waitForElement(CPA_PS1_SelectCustomerBtn);
   		
-  		SelectTargetCustomerTableFirstRow.click();
+  		PS1_SelectTargetCustomerTableFirstRow.click();
   		System.out.println("--- Clicked: Select Target Customer for Peer Matching Table first row");
-  		Thread.sleep(1000);
+  		Thread.sleep(5000);
   		
   		//Clicks the Select Customer button with selection
-  		SelectCustomerBtn.click();
+  		PS1_SelectCustomerBtn.click();
   		System.out.println("--- Clicked: Select Customer Button");
-  		Thread.sleep(5000);
+  		//Waits till the Comparative Peer Group Selection table gets loaded in the Peer Selection - 02
+  		waitForElement(CPA_ComparativePeerGroupSelectionTable);
+  		Thread.sleep(4000);
+  	   // Take screenshot after the page is fully ready
+        TakesScreenshot screenshot = (TakesScreenshot) driver;
+        File sourcefile = screenshot.getScreenshotAs(OutputType.FILE);
+        File screenshotPath = new File("src/test/resources/screenshots/CustomerPeerAnalysis/Selections/PeerSelections/SelectCustomerbtnWithSelection.png");
+        FileHandler.copy(sourcefile, screenshotPath);
+        //Clicking Back button of Peer Selection - 02 for redirection to Peer Selection - 01
+        WebElement PS2_BackBtn  = waitForElement(CPA_PS2_BackBtn);
+        Thread.sleep(3000);
+        PS2_BackBtn.click();
+        System.out.println("--- Clicked - Back Button of Peer Selection - 02 and got redirected to the Peer Selection - 01");
+        //Waits till the table gets loaded
+        waitForElement(CPA_PS1_SelectTargetCustomerTable);
+  		
   	    //Verify whether it landed on the Customer Peer Analysis Card - Selections - Peer Selection
 	      String currentUrl = driver.getCurrentUrl();
 	      String expectedPeerSelectionUrl = ConfigReader.cpa_peerSelection();
@@ -111,35 +147,47 @@ public class CPA_PeerSelectionPage {
 	          throw new AssertionError("--- Not Landed on the Peer Selection Screen --- Unexpected redirection :( !Current URL you're on: " + currentUrl);
 	      }
         
-  		Thread.sleep(2000);
-  		// Take screenshot after the page is fully ready
-          TakesScreenshot screenshot = (TakesScreenshot) driver;
-          File sourcefile = screenshot.getScreenshotAs(OutputType.FILE);
-          File screenshotPath = new File("src/test/resources/screenshots/CustomerPeerAnalysis/Selections/PeerSelections/SelectCustomerbtnWithSelection.png");
-          FileHandler.copy(sourcefile, screenshotPath);
-          
-          driver.navigate().back(); //temporary but need to code for the flow till creation of scenario
-          Thread.sleep(6000);
+//          driver.navigate().back(); //temporary but need to code for the flow till creation of scenario
+          Thread.sleep(4000);
   		
   	}
   	
   	//Clicks the Select Customer button without selection
   	public void selectCustomerBtnWithOutSelection() throws IOException, InterruptedException{
   	    //waits for the Select Customer button to get load
-  		WebElement SelectCustomerBtn = waitForElement(CPA_SelectCustomerBtn);
+  		WebElement PS1_SelectCustomerBtn = waitForElement(CPA_PS1_SelectCustomerBtn);
   		//Clicks the Create Sensitivity Scenario button
-  		SelectCustomerBtn.click();
+  		PS1_SelectCustomerBtn.click();
   		System.out.println("--- Clicked: Select Customer Button without selecting a record");
-        Thread.sleep(2000);
+        Thread.sleep(5000);
   		// Take screenshot after the page is fully ready
           TakesScreenshot screenshot = (TakesScreenshot) driver;
           File sourcefile = screenshot.getScreenshotAs(OutputType.FILE);
           File screenshotPath = new File("src/test/resources/screenshots/CustomerPeerAnalysis/Selections/PeerSelections/SelectCustomerbtnWithOutSelection.png");
           FileHandler.copy(sourcefile, screenshotPath);
-          Thread.sleep(2000);
+         
   	}
-    
-    
+  	
+  //Select Target Customer for Peer Matching table Record Download
+  	public void tableRecordsDownload() throws IOException, InterruptedException{
+  		//Download button present in the table
+  		WebElement PS1_DownloadBtn_SelectTargetCustomerTable = waitForElement(CPA_PS1_DownloadBtn_SelectTargetCustomerTable);
+  		PS1_DownloadBtn_SelectTargetCustomerTable.click();
+  		System.out.println("--- Clicked - Download Button of the Select Target Customer for Peer Matching table");
+  		Thread.sleep(5000);
+  		
+  	}
+    //Clicks the Back button of Peer Selection - 01 and get redirected to Branch/Customer Page
+  	public void redirectionTo() throws IOException, InterruptedException{
+  		//Back button
+  		WebElement PS1_BackBtn = waitForElement(CPA_PS1_BackBtn);
+  		PS1_BackBtn.click();
+  		System.out.println("--- Clicked - Back Button of Peer Selection - 01 and got redirected to the Branch/Customer Selection Screen");
+  		waitForElement(CPA_GeographicalMap);
+  		Thread.sleep(5000);
+  		
+  	}
+  	
     
     
     
--- WPC_UI_Automation/src/test/java/stepdefinitions/CPA_PeerSelectionSteps.java
+++ WPC_UI_Automation/src/test/java/stepdefinitions/CPA_PeerSelectionSteps.java
@@ -53,25 +53,21 @@ public class CPA_PeerSelectionSteps {
 
 	@Then("the user should be able to select a customer record from the table and click the Select Customer button, which redirects to the Peers Selection screen only if one record is selected")
 	public void the_user_should_be_able_to_select_a_customer_record_from_the_table_and_click_the_select_customer_button_which_redirects_to_the_peers_selection_screen_only_if_one_record_is_selected() throws IOException, InterruptedException {
-	    // Write code here that turns the phrase above into concrete actions
 		cpa_peerSelections.selectCustomerBtnWithSelection();
 	}
 	
 	@When("user clicks on the Select Customer button without selecting any records, an error message should be displayed")
 	public void user_clicks_on_the_select_customer_button_without_selecting_any_records_an_error_message_should_be_displayed() throws IOException, InterruptedException {
-	    // Write code here that turns the phrase above into concrete actions
 		cpa_peerSelections.selectCustomerBtnWithOutSelection();
 	}
 
 	@Then("the user should be able to click on the Download icon present in the table header, which downloads the table records as an Excel file")
-	public void the_user_should_be_able_to_click_on_the_download_icon_present_in_the_table_header_which_downloads_the_table_records_as_an_excel_file() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	public void the_user_should_be_able_to_click_on_the_download_icon_present_in_the_table_header_which_downloads_the_table_records_as_an_excel_file() throws IOException, InterruptedException {
+		cpa_peerSelections.tableRecordsDownload();
 	}
 
 	@Then("the user should be able to click the Back button, which then redirects to the Branch\\/Customer Selection page")
-	public void the_user_should_be_able_to_click_the_back_button_which_then_redirects_to_the_branch_customer_selection_page() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	public void the_user_should_be_able_to_click_the_back_button_which_then_redirects_to_the_branch_customer_selection_page() throws IOException, InterruptedException {
+		cpa_peerSelections.redirectionTo();
 	}
 }
